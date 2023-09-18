232d1bfbf6e24b1ea45f07a4e2b1c74f82e37888
Aasish Virjala <aasish@ucsb.edu>
2023-09-17 23:59:33 -0700

    changed port

--- app.js
+++ app.js
@@ -42,6 +42,6 @@ app.post('/subscribe', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, console.log(`Server started on ${PORT}`));
